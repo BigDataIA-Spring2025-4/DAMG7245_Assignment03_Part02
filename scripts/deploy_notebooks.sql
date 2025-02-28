528ad0dd6fa4f3a7c1dd78ba2c25da13a2c4f7df
Abhinav Gangurde <gangurde.a@northeastern.edu>
2025-02-28 07:29:51 -0500

    Update deploy_notebooks.sql

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -1,17 +1,5 @@
 --!jinja
 
-/*-----------------------------------------------------------------------------
-Hands-On Lab: Intro to Data Engineering with Notebooks
-Script:       deploy_notebooks.sql
-Author:       Jeremiah Hansen
-Last Updated: 6/11/2024
------------------------------------------------------------------------------*/
-
--- See https://docs.snowflake.com/en/LIMITEDACCESS/execute-immediate-from-template
-
--- Create the Notebooks
---USE SCHEMA {{env}}_SCHEMA;
-
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."{{env}}_{{schema1}}"."{{env}}_01_load_files"')
     FROM '@"FRED_DB"."INTEGRATIONS"."FRED_GIT_REPO"/branches/"{{branch}}"/notebooks/01_load_files/'
     QUERY_WAREHOUSE = 'FRED_WH'
